c31b8d262fab155c944329ac7415c672df11587b
FR_Jess <fr_jess@icloud.com>
2022-11-11 15:21:27 +0100

    [add] mail cycle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+// **Bonus**
+// Creare un loading e far comparire gli indirizzi email solamente quando sono stati TUTTI generati.
+
 // VARIABLES DECLARATION AND INIZIALIZATION
 
 // FUNCTIONS
@@ -9,14 +16,39 @@ const {createApp} = Vue;
 createApp({
 
   data(){
+    return{
+      title: 'Mailing list',
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      mails: [],
+      numberOfMail: 10,
+      isLoaded: false,
+    }
 
   },
 
   methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then( result => {
+        console.log(result.data);
+        this.mails.push(result.data.response);
+        this.isLoaded = true;
+      })
+    },
+
+    mailList(number){
+      for (i = 0; i < number; i++){
+        this.getApi();
+      }
+
+    }
 
   },
 
+
+
   mounted(){
+    this.mailList(this.numberOfMail);
 
   }
 }).mount('#app')
